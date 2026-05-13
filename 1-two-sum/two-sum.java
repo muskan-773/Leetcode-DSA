ac6d6a2ad61773557a97afdc9ecc86ee16d640fa
Muskan Kumari <muskankumari7371039@gmail.com>
2026-05-13 07:41:07 +0530

    Time: 2 ms (99.36%) | Memory: 47.2 MB (23.78%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,38 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
+        HashMap<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target)
-                    return new int[] { i, j };
-            }
+            int need = target - nums[i];
+            if (mp.containsKey(need))
+                return new int[] { mp.get(need), i };
+            mp.put(nums[i], i);
         }
-        return new int[] { -1, -1 };
+        return new int[] {};
     }
 }
-//TC = O(N^2)
-//SC = O(1);
+
+//TC = O(N)
+//SC = O(N);
+
+// Dry run :-> nums = [2, 7, 11, 15], target = 9
+
+// Initial:
+
+// map = {}
+// i = 0
+
+// nums[i] = 2
+// need = 9 - 2 = 7
+// map does not contain 7
+// store 2 -> 0
+// map = {2=0}
+// i = 1
+
+// nums[i] = 7
+// need = 9 - 7 = 2
+// map contains 2
+// return [0, 1]
+// Result:
+
+// indices = [0, 1]
