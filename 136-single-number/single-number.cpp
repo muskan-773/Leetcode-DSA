94410e4e718bbfd3e8073e721403ffe1f833e51f
Muskan Kumari <muskankumari7371039@gmail.com>
2026-06-14 17:50:28 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (57.52%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int j = 0;
+        for(int i = 0;i < nums.size();i++){
+            j ^= nums[i];
+        }
+        return j;
+    }
+};
