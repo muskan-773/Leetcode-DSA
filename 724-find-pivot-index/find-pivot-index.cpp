a602024f582f53ecc8aa9b109044378914611e9b
Muskan Kumari <muskankumari7371039@gmail.com>
2026-06-15 08:16:38 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (79.18%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            total -= nums[i];
+            if(total == leftSum) return i;
+            leftSum += nums[i];
+        }
+        return - 1;
+    }
+};
