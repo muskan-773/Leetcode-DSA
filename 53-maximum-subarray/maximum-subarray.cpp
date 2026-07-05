f3e2100421e4319ffa708664f4bea7961cd5b161
Muskan Kumari <muskankumari7371039@gmail.com>
2026-07-05 09:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (14.49%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,7 +5,7 @@ public:
         int sum = 0;
         for(int i = 0;i < nums.size();i++){
             sum += nums[i];
-            maxSum = max(sum, maxSum);
+            maxSum = max(maxSum, sum);
             if(sum < 0) sum = 0;
         }
         return maxSum;
